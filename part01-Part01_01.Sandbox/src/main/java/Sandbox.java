3d66baaca862c52f5566bf1793c79cc23d54fbd1
Matis141 <83648971+Matis141@users.noreply.github.com>
2023-09-18 22:09:25 +0200

    Add files via upload

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,12 @@
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Hello world!");
+        int first = 5;
+        int second = 2;
+        double result = first / second;
+        System.out.println(result);        
+    }
+}
